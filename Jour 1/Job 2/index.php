ded4f7a2ad6f6c60e77aedebabe8995d3b46d9e7
Otman <otman.tourbi@laplateforme.io>
2025-01-14 17:53:52 +0100

    Add files via upload

---
+++ Jour 1/Job 2/index.php
@@ -0,0 +1,7 @@
+<?php
+// Affiche "Hello LaPlateforme!" une première fois
+echo "Hello LaPlateforme!";
+
+/* Affiche "Hello LaPlateforme!" une deuxième fois */
+echo "Hello LaPlateforme!";
+?>
---
+++ Jour 1/Job 3/index.php
@@ -0,0 +1,15 @@
+<?php
+$bool = true;
+$int = 42;
+$str = "LaPlateforme";
+$float = 3.14;
+
+echo $bool;
+echo "\n";
+echo $int;
+echo "\n";
+echo $str;
+echo "\n";
+echo $float;
+echo "\n";
+?>
